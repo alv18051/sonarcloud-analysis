19a8d6a2a1ae64eaa55d6db6a5b94910c7b58beb
diego6ruiz <diego6ruiz@gmail.com>
2024-05-28 19:30:15 -0600

    prueba

--- main.py
+++ main.py
@@ -17,9 +17,6 @@ def write_file(file_path, data):
 
 
 def get_user_input():
-
-    api_key = "12345-secure-key"
-    print("Using API Key: " + api_key)
     user_input = input("Enter some text: ")
     return user_input
 
